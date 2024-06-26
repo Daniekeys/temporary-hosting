115285b190789ae42045749e2e3dac1988293a54
daniekeys <starlordflash@gmail.com>
2024-06-26 20:49:29 +0100

    added the faq data

--- utils/faqdata.ts
+++ utils/faqdata.ts
@@ -1,43 +1,44 @@
 export const faqData = [
   {
-    title: "Does WeCollect have a mobile app?",
+    title: "What is WeCollect app used for?",
     story:
-      "Mauris pulvinar lobortis placerat nibh elementum. Sem vitae lacus in at eget felis. Nisi donec id nisl molestie. Metus id eget sed eget. Vestibulum luctus leo euismod porttitor pulvinar vulputate volutpat arcu aliquet. Massa egestas sapien velit integer sed. Fames odio aliquet fermentum lectus nullam ",
+      "The Wecollect App helps you and your business collect data on different disciplines and query across Nigeria.",
     isLast: false,
     list: [],
   },
   {
-    title: "Lorem ipsum dolor sit amet consectetur. Gravida lectus convallis?",
+    title: "How do I create an account?",
     story:
-      "Mauris pulvinar lobortis placerat nibh elementum. Sem vitae lacus in at eget felis. Nisi donec id nisl molestie. Metus id eget sed eget. Vestibulum luctus leo euismod porttitor pulvinar vulputate volutpat arcu aliquet. Massa egestas sapien velit integer sed. Fames odio aliquet fermentum lectus nullam ",
+      "Firstly, install the WeCollect app on your android or iOS mobile phone.Create an account with just your full name and phone number ",
     isLast: false,
     list: [],
   },
   {
-    title: "Lorem ipsum dolor sit amet consectetur. Gravida lectus convallis?",
+    title: `Can I customize the questions for my
+project?`,
     story:
-      "Mauris pulvinar lobortis placerat nibh elementum. Sem vitae lacus in at eget felis. Nisi donec id nisl molestie. Metus id eget sed eget. Vestibulum luctus leo euismod porttitor pulvinar vulputate volutpat arcu aliquet. Massa egestas sapien velit integer sed. Fames odio aliquet fermentum lectus nullam ",
+      "Yes you can customize the questions on the platform to suit your project query. ",
     isLast: false,
     list: [],
   },
   {
-    title: "Lorem ipsum dolor sit amet consectetur. Gravida lectus convallis?",
-    story:
-      "Mauris pulvinar lobortis placerat nibh elementum. Sem vitae lacus in at eget felis. Nisi donec id nisl molestie. Metus id eget sed eget. Vestibulum luctus leo euismod porttitor pulvinar vulputate volutpat arcu aliquet. Massa egestas sapien velit integer sed. Fames odio aliquet fermentum lectus nullam ",
+    title: "Is my data secure and private?",
+    story: "Yes your data is secure on the platform. ",
     isLast: false,
     list: [],
   },
   {
-    title: "Lorem ipsum dolor sit amet consectetur. Gravida lectus convallis?",
+    title: `Can I collaborate with others on a data collection project?`,
     story:
-      "Mauris pulvinar lobortis placerat nibh elementum. Sem vitae lacus in at eget felis. Nisi donec id nisl molestie. Metus id eget sed eget. Vestibulum luctus leo euismod porttitor pulvinar vulputate volutpat arcu aliquet. Massa egestas sapien velit integer sed. Fames odio aliquet fermentum lectus nullam ",
+      "Yes you can collaborate with others. All you have to do is share your organisation ID with your field agents who would be collecting your data ",
     isLast: false,
     list: [],
   },
   {
-    title: "Lorem ipsum dolor sit amet consectetur. Gravida lectus convallis?",
+    title:
+      "How do I add different types of questions (multiple-choice, open-ended, etc)?",
     story:
-      "Mauris pulvinar lobortis placerat nibh elementum. Sem vitae lacus in at eget felis. Nisi donec id nisl molestie. Metus id eget sed eget. Vestibulum luctus leo euismod porttitor pulvinar vulputate volutpat arcu aliquet. Massa egestas sapien velit integer sed. Fames odio aliquet fermentum lectus nullam ",
+      "Our template is pre-designed in a flexible way that you could customised your respondent’s response type as “short answer”, “paragraph”, multiple-choice, date, time, etc. ",
     isLast: false,
     list: [],
     // list: [
@@ -47,10 +48,90 @@ export const faqData = [
     // ],
   },
   {
-    title: "Lorem ipsum dolor sit amet consectetur. Gravida lectus convallis?",
+    title: "Is there an option for agents to saveprogress and continue later?",
     story:
-      "Mauris pulvinar lobortis placerat nibh elementum. Sem vitae lacus in at eget felis. Nisi donec id nisl molestie. Metus id eget sed eget. Vestibulum luctus leo euismod porttitor pulvinar vulputate volutpat arcu aliquet. Massa egestas sapien velit integer sed. Fames odio aliquet fermentum lectus nullam ",
+      "Yes. Data is automatically saved on our server in real time. This makes it possible for agents to continue from where they left off when they return. ",
     isLast: false,
     list: [],
   },
+  {
+    title: `How do I share my project with
+participants?`,
+    story:
+      "You can share your project to your field agents by adding the agents you wish to use on your project. WeCollect would then send them a link to your projects. On installing WeCollect mobile app, field agents would have access to your project. ",
+    isLast: false,
+    list: [],
+  },
+  {
+    title: `Can I export the collected data in
+different formats (excel. CSV. etc)?`,
+    story:
+      "Yes. your data report can be exported in CSV format from the project manager’s dashboard. ",
+    isLast: false,
+    list: [],
+  },
+  {
+    title: `Is there a limit to the number of
+responses I can collect?`,
+    story:
+      "The minimum or maximum number of responses to be collected is determined by the project owner/manager. ",
+    isLast: false,
+    list: [],
+  },
+  {
+    title: `Is there an offline mode for data
+collection?`,
+    story:
+      "Yes. Field agents can collect data offline or in situations where the network is poor. Data is automatically submitted to the server when the network is restored. ",
+    isLast: false,
+    list: [],
+  },
+  {
+    title: `How do I view the
+collected data within the app?`,
+    story:
+      "You can view collected data within the app from the “Data” page on the slide menu on the mobile app. ",
+    isLast: false,
+    list: [],
+  },
+  {
+    title: `Are there any analytics or reporting
+features available?`,
+    story:
+      "Yes. There are basic analytics on the project manager’s dashboard which gives a comprehensive report of the data that has been collected. ",
+    isLast: false,
+    list: [],
+  },
+  {
+    title: `What happens if I accidentally delete a
+survey or data?`,
+    story:
+      "Write Us at info@wecollect.tech and we will recover the data you deleted for you. ",
+    isLast: false,
+    list: [],
+  },
+  {
+    title: `Is the app available on both iOS and
+Android?`,
+    story:
+      "The mobile apps are available for download on Google play and App stores. ",
+    isLast: false,
+    list: [],
+  },
+  {
+    title: `How do I manage field agents’
+responses and data?`,
+    story: "You can decide to approve, decline or export submitted data. ",
+    isLast: false,
+    list: [],
+  },
+  {
+    title: `Is there a customer support or help
+centre available?`,
+    story:
+      "In a case of incorrect or incomplete data, the project manager can decline such data. ",
+    isLast: false,
+    list: [],
+  },
+
 ];
