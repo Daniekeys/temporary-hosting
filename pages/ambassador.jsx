3a6b9964a2b3c2512af3e095f5b159eadbb51a60
daniekeys <starlordflash@gmail.com>
2024-09-20 16:13:07 +0100

    updated

--- pages/ambassador.jsx
+++ pages/ambassador.jsx
@@ -2,17 +2,21 @@ import Footer from "../components/LandingPage/Footer"
 import Navbar from "../components/LandingPage/Navbar"
 import AmbHero from "../components/ambassadors/AmbHero"
 import Register from "../components/ambassadors/Register"
+import NewFooter from "../components/footer/new-footer"
+import MobileNav from "../components/navbar/mobile-nav"
+import NewNavbar from "../components/navbar/new-navbar"
 
 
 const Ambasador = () => {
   return (
     <div className="w-full flex flex-col">
-          <Navbar active={true} />
-          <AmbHero />
-          <Register />
-          <Footer />
+      <NewNavbar />
+      <MobileNav present={1} />
+      <AmbHero />
+      <Register />
+      <NewFooter />
     </div>
-  )
+  );
 }
 
 export default Ambasador
